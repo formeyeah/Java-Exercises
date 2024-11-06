ec55caa54a6e66a19b47914591bc13875e6d03d3
formeyeah <156561169+formeyeah@users.noreply.github.com>
2024-11-06 06:11:28 +0330

    Add files via upload

---
+++ day0/src/day0/problem3/Problem3.java
@@ -0,0 +1,15 @@
+package day0.problem3;
+
+public class Problem3 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			if (i==20) {
+				System.out.println("my code found that i is: "+i + " ");
+				break;
+			}
+		}
+		
+	}
+
+}
---
+++ day0/src/day0/problem7/Account.java
@@ -0,0 +1,5 @@
+package day0.problem7;
+
+public class Account {
+
+}
