924a76724cf5aca3234e620cafc569e208f22c3f
formeyeah <mediumformeyeah@gmail.com>
2024-11-07 19:42:03 +0330

    updated problem7

--- day0/src/day0/problem7/Problem7.java
+++ day0/src/day0/problem7/Problem7.java
@@ -3,7 +3,11 @@ package day0.problem7;
 public class Problem7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Account rojina= new Account(100);
+		rojina.printInfo();
+		rojina.withdraw(40);
+		rojina.printInfo();
+		
 
 	}
 
