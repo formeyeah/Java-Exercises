9c9b90b6787639f31c1a747853db79766c74398a
formeyeah <mediumformeyeah@gmail.com>
2024-11-23 06:22:33 +0330

    add jdbcjsonquize kamel shodeh khudam

---
+++ 8/JsonJdbcQuize/src/main/java/ir/freeland/jsonquize/model/Employee.java
@@ -0,0 +1,52 @@
+package ir.freeland.jsonquize.model;
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private String salary;
+	public Employee(String firstName, String lastName, String email, String phoneNumber, String salary) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.salary = salary;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public Employee() {
+		super();
+	}
+	
+	}
+	//@TODO make it base on json
